01a3a932a7c822262240aa8f681e48a49bdde2ab
shivananda-sonwane <shivanandasonwane321@gmail.com>
2022-09-28 17:37:43 +0530

    change app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    return "My first CI CD pipeline"
+    return "My first CI CD pipeline  changed "
 
 if __name__=="__main__":
     app.run(debug=True)
